6e8cbf1dae499ef2d3d4c9cf2d30c272570e7f92
EduardoMiyasakii <eduardo.miyasaki@sptech.school>
2025-09-03 18:38:21 -0300

    Regex de CPF

---
+++ pre-desenvolvimento/src/utils/regexCPF.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+    const input = document.getElementById("cpf");
+
+    input.addEventListener("input", function (e) {
+       
+        let value = e.target.value.replace(/\D/g, ""); // só números
+
+        if (value.length > 11) value = value.substring(0, 11); // máximo 11 dígitos
+
+         if (value.length == 11) {
+            value = value.replace(/^(\d{3})(\d{3})(\d{3})(\d{2})$/, "$1.$2.$3-$4"); // celular
+        }
+
+        e.target.value = value;
+    });
+}
+
+
+
